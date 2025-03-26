fb063ec3ab95dc7012329f6fdb4412e7a2f7f502
idk-kid1214 <archer.e.woodard@gmail.com>
2025-03-25 18:51:41 -0700

    Update GUI.lua

--- GUI.lua
+++ GUI.lua
@@ -292,7 +292,6 @@ local function onCharacterAdded(character)
     if humanoid then
         humanoid.Died:Connect(function()
             player.CharacterAdded:Wait() -- Wait for the new character to spawn
-            task.wait(1) -- Ensure full respawn process is fully complete
             loadstring(game:HttpGet('https://raw.githubusercontent.com/idk-kid1214/SB-GUI/refs/heads/main/GUI.lua'))()
         end)
     end
